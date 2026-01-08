758b6bfdfa06c9aba152d79aae39f0f9dc1bde4e
Mohanad <mohandahmed015529.222.00@gmail.com>
2026-01-08 17:49:42 +0200

    First

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { HelmetProvider } from 'react-helmet-async';
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -65,6 +66,7 @@ const App = () => (
           </AuthProvider>
         </DataProvider>
       </BrowserRouter>
+      <SpeedInsights/>
     </TooltipProvider>
     </HelmetProvider>
   </QueryClientProvider>
